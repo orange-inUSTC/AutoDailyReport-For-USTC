acb8a8956d4caf6955be25fc075dd584f3dc7ae3
orange-inUSTC <107188134+orange-inUSTC@users.noreply.github.com>
2022-06-09 19:43:39 +0800

    Update newtime.py

--- newtime.py
+++ newtime.py
@@ -2,6 +2,7 @@ import cv2
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import time
+from random import randint
 
 #  行程
 bbox = [435, 713, 808, 755, 42]  # 行程码时间位置(:4)以及字体大小(-1)
@@ -12,7 +13,7 @@ im = Image.new("RGB", (shp[0] + 20, shp[1]), (254, 254, 254))
 dr = ImageDraw.Draw(im)
 font = ImageFont.truetype("HarmonyOS_Sans_Regular.ttf", bbox[4])
 
-dr.text((0, 0), time.strftime("%Y.%m.%d %H:%M:%S", time.localtime(time.time()+8*3600 - 45)), color,
+dr.text((0, 0), time.strftime("%Y.%m.%d %H:%M:%S", time.localtime(time.time()+8*3600 - randint(45,75))), color,
         font)
 img[bbox[1]:bbox[3], bbox[0]:bbox[2], :] = cv2.resize(np.array(im), shp)
 cv2.imwrite("xcm.jpg", img)
